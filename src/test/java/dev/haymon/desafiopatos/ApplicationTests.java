76ab057941aa86ff24d06727dd54638ea3aa435c
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-28 22:45:49 -0400

    Projeto criado

---
+++ src/test/java/dev/haymon/desafiopatos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.haymon.desafiopatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
