234dfc7fffb4421699d5eca5b2eaf540ec188702
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-29 23:45:20 -0400

    Habilitando auditoria do JPA

--- src/main/java/dev/haymon/desafiopatos/Application.java
+++ src/main/java/dev/haymon/desafiopatos/Application.java
@@ -2,8 +2,10 @@ package dev.haymon.desafiopatos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
