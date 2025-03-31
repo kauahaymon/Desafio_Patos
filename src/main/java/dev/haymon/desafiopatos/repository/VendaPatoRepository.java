09bcb587e923704e15469fba26f33b08d65a81d4
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-30 20:09:17 -0400

    Criando repositorios para entidades: Venda e VendaPato

---
+++ src/main/java/dev/haymon/desafiopatos/repository/VendaPatoRepository.java
@@ -0,0 +1,7 @@
+package dev.haymon.desafiopatos.repository;
+
+import dev.haymon.desafiopatos.model.VendaPato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendaPatoRepository extends JpaRepository<VendaPato, Long> {
+}
---
+++ src/main/java/dev/haymon/desafiopatos/repository/VendaRepository.java
@@ -0,0 +1,7 @@
+package dev.haymon.desafiopatos.repository;
+
+import dev.haymon.desafiopatos.model.Venda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendaRepository extends JpaRepository<Venda, Long> {
+}
